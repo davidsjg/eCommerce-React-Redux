295d4bda980ab96627b4f1ba69ffdbf5346165fe
Joey Davidson <j.davidson18@gmail.com>
2021-12-31 03:04:26 -0700

    Mongo connected but issue with backend PORT

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3051;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
