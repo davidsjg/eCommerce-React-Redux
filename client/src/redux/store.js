8559431e7f5bf776827644b87a6dcf5a7fd4ea08
Joey Davidson <j.davidson18@gmail.com>
2022-01-01 05:53:14 -0700

    reducers and actions complete

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,7 +2,17 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const reducer = combineReducers({});
+import { cartReducers } from "./reducers/cartReducers";
+import {
+  getProductsReducer,
+  getProductDetailsReducer,
+} from "./reducers/productReducers";
+
+const reducer = combineReducers({
+  cart: cartReducers,
+  getProducts: getProductsReducer,
+  getProductDetails: getProductDetailsReducer,
+});
 
 const middleware = [thunk];
 
